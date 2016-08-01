c36dfd33799a99c59e7a0411ab427893061b5189
biciclista <biciclista@gmail.com>
2016-08-01 20:57:07 +0200

    Other commit

--- app/src/androidTest/java/com/irf/profiles/model/VolumePropertyTest.java
+++ app/src/androidTest/java/com/irf/profiles/model/VolumePropertyTest.java
@@ -36,7 +36,56 @@ public class VolumePropertyTest {
         // Create property not active, set a different volume and apply.
         VolumeProperty property = new VolumeProperty(1L, 1L, VolumeProperty.VOL_ALARM, 0, false);
         property.setValue(alarmVolume == 0 ? 1 : 0);
+        property.apply(mContext);
 
         assertEquals(alarmVolume, mAudioManager.getStreamVolume(AudioManager.STREAM_ALARM));
+    }
+
+    @Test
+    public void applyAlarm() {
+        // Volume to set.
+        int alarmVolume = (mAudioManager.getStreamVolume(AudioManager.STREAM_ALARM) == 0 ? 1 : 0);
+
+        // Create property active, set volume and apply.
+        VolumeProperty property = new VolumeProperty(1L, 1L, VolumeProperty.VOL_ALARM, alarmVolume, true);
+        property.apply(mContext);
+
+        assertEquals(alarmVolume, mAudioManager.getStreamVolume(AudioManager.STREAM_ALARM));
+    }
+
+    @Test
+    public void applyRing() {
+        // Volume to set.
+        int alarmVolume = (mAudioManager.getStreamVolume(AudioManager.STREAM_RING) == 0 ? 1 : 0);
+
+        // Create property active, set volume and apply.
+        VolumeProperty property = new VolumeProperty(1L, 1L, VolumeProperty.VOL_RING, alarmVolume, true);
+        property.apply(mContext);
+
+        assertEquals(alarmVolume, mAudioManager.getStreamVolume(AudioManager.STREAM_RING));
+    }
+
+    @Test
+    public void applyNotification() {
+        // Volume to set.
+        int alarmVolume = (mAudioManager.getStreamVolume(AudioManager.STREAM_NOTIFICATION) == 0 ? 1 : 0);
+
+        // Create property active, set volume and apply.
+        VolumeProperty property = new VolumeProperty(1L, 1L, VolumeProperty.VOL_NOTIFICATION, alarmVolume, true);
+        property.apply(mContext);
+
+        assertEquals(alarmVolume, mAudioManager.getStreamVolume(AudioManager.STREAM_NOTIFICATION));
+    }
+
+    @Test
+    public void applyMultimedia() {
+        // Volume to set.
+        int alarmVolume = (mAudioManager.getStreamVolume(AudioManager.STREAM_MUSIC) == 0 ? 1 : 0);
+
+        // Create property active, set volume and apply.
+        VolumeProperty property = new VolumeProperty(1L, 1L, VolumeProperty.VOL_MEDIA, alarmVolume, true);
+        property.apply(mContext);
+
+        assertEquals(alarmVolume, mAudioManager.getStreamVolume(AudioManager.STREAM_MUSIC));
     }
 }
